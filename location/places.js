c61b316be42dbfd21500c01043f6972347f77588
cyang <cyellowgreen@gmail.com>
2023-03-17 16:55:12 +0900

    click event add

--- location/places.js
+++ location/places.js
@@ -13,11 +13,37 @@ window.onload = () => {
               y: 20,
               z: 20
           });
-          entity.setAttribute('material', { color: 'red' } );
+          // entity.setAttribute('material', { color: 'red' } );
+          entity.setAttribute('src', 'https://previews.123rf.com/images/giamportone/giamportone1711/giamportone171100568/90514900-gps-%EC%9C%84%EC%B9%98-%EB%A7%88%EC%BB%A4-%EC%9C%84%EC%B9%98-%EC%95%84%EC%9D%B4%EC%BD%98-%EC%A7%80%EB%8F%84-%ED%95%80-%EC%98%A4%EB%A0%8C%EC%A7%80-%EC%95%84%EC%9D%B4%EC%BD%98.jpg');  
           entity.setAttribute('gps-new-entity-place', {
               latitude: e.detail.position.latitude + 0.001,
               longitude: e.detail.position.longitude
           });
+      const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('gps-new-entity-place');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+
+        entity.addEventListener('click', clickListener);
+
+
           document.querySelector("a-scene").appendChild(entity);
       }
       testEntityAdded = true;
